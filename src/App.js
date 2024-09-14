d280dbc863d684127b4f6c2989e44c0046b134a6
ZXINNATTAPAT <bigboyzin88@gmail.com>
2024-09-14 21:56:42 +0700

    new edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  // const c = "hello"
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+        <div className="card">
+          <div className="card-body">
+            <h1>web frame work Ep xxc</h1>
+          </div>  
+        </div>
     </div>
   );
 }
